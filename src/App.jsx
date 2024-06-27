9504ff10f7bf3071ffea38032b022a6617d6b1c8
dhanush-2313 <dhanusu006@gmail.com>
2024-06-27 22:44:08 +0530

    firstCommit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <>
+      <Button variant='outlined'>CLick me</Button>
+    </>
+  )
+}
+
+export default App
